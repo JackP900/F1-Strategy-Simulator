c3450fda9ec46b907e570a2afacc19e6120799f0
Jack page <jpage2603@gmail.com>
2026-03-31 16:32:42 +0100

    31/03/2026 Changed layout of website

--- F1_strategy/app.py
+++ F1_strategy/app.py
@@ -7,29 +7,63 @@ from pathlib import Path
 
 #create a title and subheader
 st.title("Formula 1 Strategy Simulator")
-st.subheader("Race settings: ")
+st.subheader("Session settings: ")
 
-#splitting the page into 3 columns and add data inputs
-col1, col2, col3 = st.columns(3, gap="medium")
+if "stage" not in st.session_state:
+    st.session_state.stage = 1
+if st.session_state.stage == 1:
+    st.subheader("Session settings: ")
 
-driver = col1.text_input("Enter Driver Abbreviation")
-round = col2.text_input("Enter round")
-session = col3.selectbox("Enter Session", ["FP1", "FP2", "FP3", "Q", "R"])
+    #splitting the page into 3 columns and add data inputs
+    col1, col2, col3 = st.columns(3, gap="medium")
+
+    driver = col1.text_input("Enter Driver Abbreviation")
+    round = col2.text_input("Enter round")
+    session = col3.selectbox("Enter Session", ["FP1", "FP2", "FP3", "Q", "R"])
+    year = col2.slider("Enter year", 2018, 2025)
+
+    if st.button("Continue"):
+        st.session_state.stage = 2
+        st.session_state.year = year
+        st.session_state.round = int(round)
+        st.session_state.driver = driver
+        st.session_state.session = session
 
 
-year = col1.slider("Enter year", 2018, 2025)
-lap_num = col2.text_input("How many laps in a race")
-base_time = col3.text_input("Base lap time")
-pit_stop = col1.text_input("How much time lost in pit stop")
-warm_up_cost = col2.text_input("Enter time lost warming up tyres")
-fuel_effect = col3.text_input("Enter time lost(s)")
+elif st.session_state.stage == 2:
+    st.subheader("Race Settings:")
 
-race_button = st.button("Run Analysis")
+    col1, col2, col3 = st.columns(3, gap="medium")
+
+    lap_num = col1.text_input("How many laps in a race")
+    base_time = col2.text_input("Base lap time")
+    pit_stop = col3.text_input("How much time lost in pit stop")
+    warm_up_cost = col1.text_input("Enter time lost warming up tyres")
+    fuel_effect = col2.text_input("Enter time lost(s)")
+
+
 #if button is pressed then it loads the these data inputs
-if race_button:
-    st.subheader("tyre Variables:")
-    session = load_race(year, int(round), session)
-    graph, results = run_analysis(session, driver)
+    if st.button("Tyre Settings"):
+        st.session_state.stage = 3
+
+elif st.session_state.stage == 3:
+        st.subheader("tyre Settings:")
+
+        col1, col2, col3 = st.columns(3, gap="medium")
+
+        start_compound = col1.selectbox("Starting tyre compound", ["Soft", "Medium", "Large"])
+        next_tyre = col2.selectbox("Next tyre compound", ["Soft", "Medium", "Large"])
+        num_stops = col3.selectbox("1 stop or 2 stop", ["1 stop", "2 stops"])
+
+        if st.button("Run Analysis"):
+            st.session_state.stage = 4
+
+elif st.session_state.stage == 4:
+    session = load_race(st.session_state.year, 
+                        st.session_state.round, 
+                        st.session_state.session)
+            
+    graph, results = run_analysis(session, st.session_state.driver)
 
     st.pyplot(graph)
 
@@ -37,11 +71,11 @@ if race_button:
     results.to_csv(outpath, index=False)
     st.dataframe(results)
 
-    col1, col2, col3 = st.columns(3, gap="medium")
 
-    start_compound = col1.selectbox("Starting tyre compound", ["Soft", "Medium", "Large"])
-    next_tyre = col2.selectbox("Next tyre compound", ["Soft", "Medium", "Large"])
-    num_stops = col3.selectbox("1 stop or 2 stop", ["1 stop", "2 stops"])
+             
+
+    
+
 
 
 
