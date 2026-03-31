28d860b5c0ab9f596b862b903ccbddda2ed6d1e7
Jack page <jpage2603@gmail.com>
2026-03-31 15:35:51 +0100

    31/03/2026 Functioning trye deg model

--- F1_strategy/analysis.py
+++ F1_strategy/analysis.py
@@ -1,65 +1,64 @@
 #estimating tyre Degradation using FastF1 library
-import fastf1
-import numpy as np
-from pathlib import Path
-from scipy.optimize import curve_fit
-from data_loader import load_race, get_driver_laps
-import matplotlib.pyplot as plt
-import pandas as pd
+def run_analysis(session, driver):
+    import fastf1
+    import numpy as np
+    from scipy.optimize import curve_fit
+    from data_loader import load_race, get_driver_laps
+    import matplotlib.pyplot as plt
+    import pandas as pd
 
-session = load_race(2019, 8, "R")
-driver = "VER"
+    def tyre_deg(x, scale, deg_rate, lap_time):
+        #calculation for tyre life
+        return scale * np.exp(deg_rate * x) + lap_time
 
-def tyre_deg(x, scale, deg_rate, lap_time):
-    #calculation for tyre life
-    return scale * np.exp(deg_rate * x) + lap_time
+    #stores data of tyres
+    results = []
+    compounds = ["SOFT", "MEDIUM", "HARD"]
 
-#stores data of tyres
-results = []
-compounds = ["SOFT", "MEDIUM", "HARD"]
-for comp in compounds:
-    laps = get_driver_laps(session, driver)
-    laps = laps[laps["Compound"] == comp]
+    graph, ax = plt.subplots()
 
-    x = laps["TyreLife"].astype(float).to_numpy()
-    y = laps["LapTimeSeconds"].to_numpy()
+    for comp in compounds:
+        laps = get_driver_laps(session, driver)
+        laps = laps[laps["Compound"] == comp]
+
+        x = laps["TyreLife"].astype(float).to_numpy()
+        y = laps["LapTimeSeconds"].to_numpy()
 
 
-    try:
-        if len(x) == 0 or len(y) == 0:
-            print(f"Skipping {comp}: no usable data")
+        try:
+            if len(x) < 5:
+                print(f"Skipping {comp}: not enough data")
+                continue
+            param, cov = curve_fit(tyre_deg, x, y, p0=(1, 0.01, np.min(y)))
+
+            scale, deg_rate, lap_time = param
+            results.append({
+                "Compound": comp,
+                "a": scale,
+                "b": deg_rate,
+                "c": lap_time,
+                "Degradation_rate": deg_rate,
+                "Lap_Time": lap_time
+            })
+        
+            xfit = np.linspace(0, x.max(), 30)
+            ax.scatter(x, y, label=f"{comp} data")
+            ax.plot(xfit, tyre_deg(xfit, *param), linestyle="--", label=f"{comp} fit")
+
+        except RuntimeError as e:
+            print(f"Could not fit {comp}: {e}")
             continue
-        param, cov = curve_fit(tyre_deg, x, y, p0=(1, 0.01, np.min(y)))
 
-        scale, deg_rate, lap_time = param
-        results.append({
-            "Compound": comp,
-            "a": scale,
-            "b": deg_rate,
-            "c": lap_time,
-            "Degradation_rate": deg_rate,
-            "Lap_Time": lap_time
-        })
-    
-        xfit = np.linspace(0, x.max(), 30)
-        plt.scatter(x, y, label=f"{comp} data")
-        plt.plot(xfit, tyre_deg(xfit, *param), label=f"{comp} fit", linestyle="--")
+        ax.set_title("Tyre Degradation Plot")
+        ax.set_xlabel("Tyre Age(laps)")
+        ax.set_ylabel("Lap Time (s)")
+        ax.invert_yaxis()
+        ax.legend()
+        ax.grid=True
 
-    except RuntimeError:
-        print(f"could not fir {comp}")
-        continue
+        df_results = pd.DataFrame(results)
 
-    plt.title("{driver} Tyre Degradation: {round} {year}")
-    plt.xlabel("Tyre Age (laps)")
-    plt.ylabel("Lap Time (s)")
-    plt.gca().invert_yaxis()
-    plt.legend()
-    plt.show()
-
-    df_results = pd.DataFrame(results)
-    outpath = Path("Tyre_Degradation_Results.csv")
-    df_results.to_csv(outpath, index=False)
-    print(df_results)
+        return graph, df_results
 
 
 
--- F1_strategy/app.py
+++ F1_strategy/app.py
@@ -1,5 +1,9 @@
 #UI for the F1 Strategy Simulator
 import streamlit as st
+from analysis import run_analysis
+from data_loader import load_race
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 #create a title and subheader
 st.title("Formula 1 Strategy Simulator")
@@ -9,21 +13,29 @@ st.subheader("Race settings: ")
 col1, col2, col3 = st.columns(3, gap="medium")
 
 driver = col1.text_input("Enter Driver Abbreviation")
-round = col1.text_input("Enter round")
-session = col2.selectbox("Enter Session", ["FP1", "FP2", "FP3", "Qualifying", "Race"])
-year = col3.slider("Enter year", 2018, 2025)
-lap_num = col1.text_input("How many laps in a race")
-base_time = col2.text_input("Base lap time")
-pit_stop = col3.text_input("How much time lost in pit stop")
-warm_up_cost = col1.text_input("Enter time lost warming up tyres")
-fuel_effect = col2.text_input("Enter time lost(s)")
-
-race_button = st.button("Continoue to tyre settings")
+round = col2.text_input("Enter round")
+session = col3.selectbox("Enter Session", ["FP1", "FP2", "FP3", "Q", "R"])
 
 
+year = col1.slider("Enter year", 2018, 2025)
+lap_num = col2.text_input("How many laps in a race")
+base_time = col3.text_input("Base lap time")
+pit_stop = col1.text_input("How much time lost in pit stop")
+warm_up_cost = col2.text_input("Enter time lost warming up tyres")
+fuel_effect = col3.text_input("Enter time lost(s)")
+
+race_button = st.button("Run Analysis")
 #if button is pressed then it loads the these data inputs
 if race_button:
     st.subheader("tyre Variables:")
+    session = load_race(year, int(round), session)
+    graph, results = run_analysis(session, driver)
+
+    st.pyplot(graph)
+
+    outpath = Path("Tyre_Degradation_Results.csv")
+    results.to_csv(outpath, index=False)
+    st.dataframe(results)
 
     col1, col2, col3 = st.columns(3, gap="medium")
 
